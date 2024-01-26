69d27db6f41316cc7febb8ad284c5717bfc93eaa
Alan <alannnchang@gmail.com>
2024-01-26 00:12:22 -0800

    reads and writes

--- include/assembler.h
+++ include/assembler.h
@@ -1,11 +1,12 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<fcntl.h>
-#include <unistd.h>
+#include<unistd.h>
 
 typedef struct scanner {
     char *file_name;
     int line_ct;
-    size_t len;
+    FILE *fp;
+    size_t buf;
     char *line;
 } scan;
--- src/assembler.c
+++ src/assembler.c
@@ -15,14 +15,22 @@ Authorized Functions
 ● exit
 */
 
+/*
+General idea:
 
+Loop until EOF:
+-Use getline to read a single line
+-Process line by tokenizing and identifying tokens
+-Translate tokens to machine code (binary or hexdecimal?)
+-Write to executable 
 
+*/
 
 int main(int ac, char **av){
     if (ac == 2){
 
-        FILE *fp = fopen(av[1], "r");
-        if (fp == NULL){
+        FILE *prog_fp = fopen(av[1], "r");
+        if (prog_fp == NULL){
             write(2, "Error opening file\n", 19);
             exit(EXIT_FAILURE);
         }
@@ -30,23 +38,36 @@ int main(int ac, char **av){
         scan scan = {
             .file_name = av[1],
             .line_ct = 0,
-            .len = 0,
+            .fp = prog_fp,
+            .buf = 0,
             .line = NULL,
         };
+
+        int exec_fd = open("exec_file", O_CREAT | O_WRONLY | O_TRUNC);
+        if (exec_fd == -1){
+            write(2, "Error writing file\n", 19);
+            exit(EXIT_FAILURE);
+        }
 
         ssize_t read;
 
         while(1){
-            read = getline(&scan.line, &scan.len, fp);
+            read = getline(&scan.line, &scan.buf, scan.fp);
             if (read == -1){
                 break;
             }
             scan.line_ct++;
-            printf("%d: %s", scan.line_ct, scan.line);
+
+            
+            printf("%d/%ld: %s", scan.line_ct, read, scan.line);
+            write(exec_fd, scan.line, read);
         }  
 
-        fclose(fp);
+        fclose(scan.fp);
+        if (close(exec_fd) == -1){
+            write(2, "Error closing file\n", 19);
+        }
         free(scan.line);
     }
-    return 0;
+    exit(EXIT_SUCCESS);
 }
